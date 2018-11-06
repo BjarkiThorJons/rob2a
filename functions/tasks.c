9f13aabd8e25bb747b4d1514a5f76a7068779fca
Kalliarolunni <gustigaur@gmail.com>
2018-11-06 14:14:46 +0000

    lettar breytingar

--- functions/tasks.c
+++ functions/tasks.c
@@ -1,3 +1,25 @@
+
+task start_by_light(){
+	int tel = 1;
+	while(1==1){
+		if(SensorValue(lightSensor) < 400 && tel==1)
+		{
+			StartTask(main_task);
+			tel = 0;
+			//StartTask(main);
+		}
+		else if(SensorValue(lightSensor) > 400)
+		{
+			StopTask(main_task);
+			motor[leftMotor] = 0;
+			motor[rightMotor] = 0;
+			motor[armMotor] = 0;
+			motor[clawMotor] = 0;
+			tel = 1;
+			//StartTask(main);
+		}
+	}
+}
 task start(){
 	while(1==1){
 		if(vexRT[Btn7D] == 1)
@@ -5,14 +27,15 @@ task start(){
 			StartTask(main_task);
 			//StartTask(main);
 		}
-		if(vexRT[Btn8D] == 1||SensorValue(Button)==1)
+		else if(vexRT[Btn8D] == 1||SensorValue(Button)==1)
 		{
 			StopTask(main_task);
+			StopTask(start_by_light);
 			motor[leftMotor] = 0;
 			motor[rightMotor] = 0;
 			motor[armMotor] = 0;
 			motor[clawMotor] = 0;
-			//StartTask(main);
 		}
+			//StartTask(main);
 	}
 }
